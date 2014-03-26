81d161e34cb240cbca9db11d803bee434d130b79
Hashini Senaratne <hashz1990@gmail.com>
2014-03-26 12:17:28 +0530

    front end fix to remove Admin menu from topnav if the user is not an admin

---
+++ Seranet.SpecM2.Web/app/layout/topnav.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'topnav';
+    angular.module('app').controller(controllerId,
+        ['$scope', 'common', '$routeParams', '$http', topnav]);
+
+    function topnav($scope, common, $routeParams, $http) {
+        var vm = this;
+        var role = "team";
+        $scope.userName = "";
+        $scope.isAdmin = "yes";
+        activate();
+
+        function activate() {
+
+            $http({ method: 'GET', url: 'security/username' }).
+                success(function (data, status, headers, config) {
+                    console.log(data);
+                    $scope.userName = data.split("\\")[1].toString().toLowerCase();
+                    
+                    $http({ method: 'GET', url: 'api/userrole/' + $scope.userName }).
+                        success(function (data, status, headers, config) {
+                            if (data == "admin") {
+                                $scope.isAdmin = "yes";
+                            }
+                            else {
+                                $scope.isAdmin = "no";
+                            }
+
+                        }).
+                        error(function (data, status, headers, config) {
+                            console.log(data);
+                        });
+
+                }).
+                error(function (data, status, headers, config) {
+                    console.log(data);
+                });
+
+
+            
+        }
+    };
+})();
